963d4eb5bcae95b9f24e0b3c5fc0f2ba3129d29f
Mark Lee <mark@eliondigital.com>
2021-07-30 21:26:13 +1200

    reconfigure

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -16,7 +16,6 @@ module.exports = {
   parserOptions: {
     project: ["tsconfig.json", "tsconfig.dev.json"],
     sourceType: "module",
-    tsconfigRootDir: __dirname,
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,4 @@
 import * as functions from "firebase-functions";
-import next from "next";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -9,19 +8,5 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-// const nextjsDistDir = ".next";
-const nextjsServer = next({
-  dev: false,
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore // due to issue with type file
-  conf: {
-    distDir: ".next",
-  },
-});
-
-
-const nextjsHandle = nextjsServer.getRequestHandler();
-
-export const nextServe = functions.https.onRequest((req, res) => {
-  return nextjsServer.prepare().then(() => nextjsHandle(req, res));
-});
+// NOTE: reconfigured functions and server based on example at:
+// https://github.com/jthegedus/firebase-gcp-examples/tree/main/functions-nextjs
