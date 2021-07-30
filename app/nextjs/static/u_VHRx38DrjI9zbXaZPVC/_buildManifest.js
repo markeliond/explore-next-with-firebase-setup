cfc26e10583858d4f5167813970b9ddc1fe0ebe0
Mark Lee <mark@eliondigital.com>
2021-07-30 21:26:58 +1200

    remove build

--- app/nextjs/static/u_VHRx38DrjI9zbXaZPVC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1e28ed0180c21eed9409.js"],"/_error":["static/chunks/pages/_error-8842bdd603648886795b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
