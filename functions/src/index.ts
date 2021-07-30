17b65d5420a2dde04a84ffc56a1ac1048530df17
Mark Lee <mark@eliondigital.com>
2021-07-30 16:10:40 +1200

    working deploy to cloud

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import next from "next";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,4 +7,26 @@ import * as functions from "firebase-functions";
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+const nextjsDistDir = ".next";
+const nextjsServer = next({
+  dev: false,
+  dir: ".",
+  conf: {
+    distDir: nextjsDistDir,
+    experimental: {
+
+    },
+    future: {
+
+    },
+  },
+});
+
+
+const nextjsHandle = nextjsServer.getRequestHandler();
+
+export const nextServe = functions.https.onRequest((req, res) => {
+  return nextjsServer.prepare().then(() => nextjsHandle(req, res));
 });
